ffd0489d6bd0763c6132e1b8eb82182160ffef6f
NikolaRaicevic <nikolar047@gmail.com>
2024-09-26 11:28:24 -0700

    Incorporating moveit

--- xarm_controller/launch/_ros2_control.launch.py
+++ xarm_controller/launch/_ros2_control.launch.py
@@ -67,8 +67,9 @@ def launch_setup(context, *args, **kwargs):
         ros2_control_node
     ]
 
-
 def generate_launch_description():
     return LaunchDescription([
         OpaqueFunction(function=launch_setup)
     ])
+
+
